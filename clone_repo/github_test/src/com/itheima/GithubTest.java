e26a15f369b33e6d0f4488545b2696a16fbaae00
kat <lhh_kral@163.com>
2019-06-16 21:34:55 +0800

    第三次修改

--- clone_repo/github_test/src/com/itheima/GithubTest.java
+++ clone_repo/github_test/src/com/itheima/GithubTest.java
@@ -6,5 +6,7 @@ public class GithubTest {
         //增加了一行注释，并打印了一句话
         System.out.println("第一次修改。。");
         System.out.println("第二次修改");
+        System.out.println("---------------------");
+        System.out.println("第三次修改");
     }
 }
