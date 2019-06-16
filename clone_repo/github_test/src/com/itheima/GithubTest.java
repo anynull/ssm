087e945a0cc47b430033683feca118770a1c6e77
kat <lhh_kral@163.com>
2019-06-16 21:30:20 +0800

    第一次修改

--- clone_repo/github_test/src/com/itheima/GithubTest.java
+++ clone_repo/github_test/src/com/itheima/GithubTest.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class GithubTest {
     public static void main(String[] args) {
         System.out.println("github仓库上传。。。。");
+        //增加了一行注释，并打印了一句话
+        System.out.println("第一次修改。。");
     }
 }
